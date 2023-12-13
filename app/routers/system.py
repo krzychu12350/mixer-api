edc3628145df4fc12fde719a753329050635beb7
Krzysztof Karaś <krzysztofkaras98@gmail.com>
2023-12-13 03:44:16 +0100

    Test26

--- app/routers/system.py
+++ app/routers/system.py
@@ -10,20 +10,11 @@ router = APIRouter(tags=["System"])
 @router.get("/status")
 def status():
     return {"ok": True, "version": __version__}
+#
+# @router.post("/multi")
+# async def mix_files(instrumental):
+#     # filePath = mixFileSounds(files, instrumental)
+#     #
+#     # return FileResponse(filePath, media_type='audio/wav')
+#     return {"instrumental": instrumental.filename}
 
-@router.post("/multi")
-async def mix_files(instrumental):
-    # filePath = mixFileSounds(files, instrumental)
-    #
-    # return FileResponse(filePath, media_type='audio/wav')
-    return {"instrumental": instrumental.filename}
-
-class Color(BaseModel):
-    r: float
-    g: float
-    b: float
-
-@app.post("/")
-def api_test(color: Color = Form(...)):
-    print(color)
-    print(type(color))
